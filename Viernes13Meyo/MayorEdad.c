e1066afe0a592beea33579bd9bbf0e21bbd73b66
Braulio <Braulito_Zamoora@outlook.com>
2026-05-06 22:45:57 -0600

    primer commit

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,60 @@
 #include <stdio.h>
+
+int main() {
+    int edad;
+
+    // Pedir al usuario que ingrese su edad
+    printf("Por favor, ingresa tu edad: ");
+    scanf("%d", &edad);
+
+    // Validar si la entrada es un número negativo
+    if (edad < 0) {
+        printf("Error: La edad no puede ser un número negativo.\n");
+    } 
+    // Validar la mayoría de edad (18 años o más)
+    else if (edad >= 18) {
+        printf("Eres mayor de edad.\n");
+    } 
+    // Si es menor de 18 y no es negativo
+    else {
+        printf("Eres menor de edad.\n");
+    }
+
+    return 0;
+}
+
+,,,,,,,,,,,,,,,,,s
+,,,,,,,,,,,,,,,,sss
+,,,,,,,,,,,,,,,,sss
+,,,,,,,,,,,,,,,sssss
+,,,,,,,,,,,,,,,sssss
+,,,,,,,,,,,,,,,sssss
+,,,,,,,,,,,,,,,sssss
+,,,,,,,,,,,,,,sssssss
+,,,,,,,,,,,,,,sssssss
+,,,,,,,,,,,,,,sssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,ss,,,,sssssssss,,,,sss
+,,,,,sss,,,,,ssssssss,,,,,sss
+,,,ssss,,,,,sssssssss,,,,,ssss
+,,,sssssssssssssssssssssssss
+,,,,,,,,,,,,,,ssssssss
+,,,,,,,,,,,,,,,ssssss
+,,,,,,,,,,,,,,,ssssss
+,,,,,,,,,,,,,,,ssssss
+,,,,,,,,,,,,,,,ssssss
+,,,,,,,,,,,,,,ssssssss
+,,,,,,,,,,,,,,,,sssss
+,,,,,,,,,,,,,sssssssss
+,,,,,,,,,,,,,,,ssssss
+17
